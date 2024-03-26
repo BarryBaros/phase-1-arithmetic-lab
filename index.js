da2ffb1e61422f26be27bcfc7b5dcb4d679aea05
Timothy Barasa <timbaros87@gmail.com>
2024-03-27 00:11:13 +0300

    Perfom aritmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+// An equation whose multiplied results will equal 62
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+
+// A random integer greater than 0
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+// An equation that calculates a remainder that is equal to 4
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+// will return 20 as the highest number in the set
+let max = Math.max(12, 4, 16, 9, 20, 14, 7);
+console.log(max);
